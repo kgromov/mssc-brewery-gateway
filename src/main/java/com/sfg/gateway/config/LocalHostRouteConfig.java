17d4519dff25ad8f992c0da6dac325ab8f469cef
Konstantin <konstantin@revimedia.com>
2022-02-09 23:33:24 +0200

    Add gateway routing for beer-order-service

--- src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
+++ src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
@@ -16,7 +16,12 @@ public class LocalHostRouteConfig {
         return builder.routes()
                 .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
                         .uri("http://localhost:8080")
-                        .id("beer-service"))
+                        .id("beer-service")
+                )
+                .route(r -> r.path("/api/v1/customers/**")
+                        .uri("http://localhost:8081")
+                        .id("beer-order-service")
+                )
                 .build();
     }
 }
